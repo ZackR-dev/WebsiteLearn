1a697eb08119847a9de64782a61a7638f2157071
Zack Rowlatt <zjrowlatt@gmail.com>
2025-02-14 23:15:06 +0000

    added dark and light mode transitions

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 const button = document.getElementById('Button1');
 const button2 = document.getElementById('Button2');
+const headerbox = document.getElementById('header-box')
 
-let isblack = false;
+
+
 
 button.addEventListener('click', function() {
     button.classList.remove('fade-in');
     button2.classList.remove('fade-out');
 
-    //if (isBlack) {
-    //  body.style.background = 'white';
-    //} else {
-    //  body.style.background = 'black';
-    //}
-    //isBlack = true
-    
     button.classList.add('fade-out');
     button2.classList.add('fade-in');
+    document.body.style.backgroundColor = "rgb(49, 49, 49)";
+    headerbox.style.backgroundColor = "rgb(140, 58, 58)";
+    
   });
+
 
 button2.addEventListener('click', function() {
     button.classList.remove('fade-out');
     button2.classList.remove('fade-in');
     button2.classList.add('fade-out');
     button.classList.add('fade-in');
+    document.body.style.backgroundColor = "rgb(189, 189, 189)";
+    headerbox.style.backgroundColor = "rgb(255, 154, 154)";
   });
