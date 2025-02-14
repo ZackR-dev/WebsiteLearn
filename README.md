4f23183bc15077664fff7a454544636844115e91
Zack Rowlatt <zjrowlatt@gmail.com>
2025-02-14 21:10:41 +0000

    comitting from terminal

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-"# WebsiteLearn" 
+This is the readme for my web project which I am using to learn website development 
--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const button = document.getElementById('Button1');
 const button2 = document.getElementById('Button2');
 
+let isblack = false;
+
 button.addEventListener('click', function() {
     button.classList.remove('fade-in');
     button2.classList.remove('fade-out');
+
+    //if (isBlack) {
+    //  body.style.background = 'white';
+    //} else {
+    //  body.style.background = 'black';
+    //}
+    //isBlack = true
+    
     button.classList.add('fade-out');
     button2.classList.add('fade-in');
   });
